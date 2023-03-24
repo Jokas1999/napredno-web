319aeeb4b9cffc2643c3f001a9700b0886bf9c72
josip platuzic <josip.platuzic@spandevacademy.onmicrosoft.com>
2023-03-24 21:59:33 +0100

    LV2

---
+++ 2. zad/dbcreate.php
@@ -0,0 +1,17 @@
+
+<?php
+
+function createTable(){
+    $pdo = new PDO('mysql:dbname=database;host=localhost', 'root', '');
+
+    $sql = "CREATE TABLE IF NOT EXISTS `documents` (
+        `id` int(11) PRIMARY KEY AUTO_INCREMENT,
+        `name` varchar(128) NOT NULL UNIQUE,
+        `iv` varchar(128) NOT NULL
+        ) ENGINE=InnoDB ;";
+
+    $pdo->exec($sql);
+
+    $pdo = null;
+    unset($pdo);
+}
