3d6cf46027d7d65bf3cf4db2792a883fb6666f96
josip platuzic <josip.platuzic@spandevacademy.onmicrosoft.com>
2023-04-27 00:05:23 +0200

    LV5

--- README.md
+++
@@ -1,1 +0,0 @@
-# napredno-web
---
+++ radovi/database/seeders/UserSeeder.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            [
+                'id' => '1',
+                'name' => 'admin',
+                'email' => 'admin@gmail.com',
+                'email_verified_at' => now(),
+                'password' => Hash::make('password'),
+                'remember_token' => Str::random(10),
+                'created_at' => now(),
+                'updated_at' => now(),
+                'role_id' => Role::ADMIN
+            ],
+            [
+                'id' => '2',
+                'name' => 'Josip',
+                'email' => 'josip@gmail.com',
+                'email_verified_at' => now(),
+                'password' => Hash::make('password'),
+                'remember_token' => Str::random(10),
+                'created_at' => now(),
+                'updated_at' => now(),
+                'role_id' => Role::STUDENT
+            ],
+            [
+                'id' => '3',
+                'name' => 'user',
+                'email' => 'user@gmail.com',
+                'email_verified_at' => now(),
+                'password' => Hash::make('password'),
+                'remember_token' => Str::random(10),
+                'created_at' => now(),
+                'updated_at' => now(),
+                'role_id' => Role::STUDENT
+            ],
+            [
+                'id' => '5',
+                'name' => 'Nastavnik',
+                'email' => 'nastavnik@gmail.com',
+                'email_verified_at' => now(),
+                'password' => Hash::make('password'),
+                'remember_token' => Str::random(10),
+                'created_at' => now(),
+                'updated_at' => now(),
+                'role_id' => Role::TEACHER
+            ],
+
+        ]);
+    }
+}
